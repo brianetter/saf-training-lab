1a1de57c12f20c4f31c517f8d1892a06f2226ed0
Brian Etter <136741795+brianetter@users.noreply.github.com>
2025-04-25 20:26:44 +0000

    At lesson 6

--- my_nginx/controls/example.rb
+++ my_nginx/controls/example.rb
@@ -4,25 +4,31 @@ control 'nginx-version' do
   desc 'The required version of NGINX should be installed.'
   describe nginx do
     its('version') { should cmp >= input('nginx_version') }
+    # its('version') { should cmp >= '1.27.0' }
   end
 end
 
 control 'nginx-modules' do
   impact 1.0
   title 'NGINX modules'
-  desc 'The required NGINX modules should be installed.'
-  required_modules = input('nginx_modules')
+  desc 'The required NGINX modules should be installed'
+
+  nginx_modules = input('nginx_modules')
+
   describe nginx do
-    required_modules.each do |required_module|
-      its('modules') { should include required_module }
+    # its('modules') { should include 'http_ssl' }
+    # its('modules') { should include 'stream_ssl' }
+    # its('modules') { should include 'mail_ssl' }
+    nginx_modules.each do |current_module|
+      its('modules') { should include current_module }
     end
   end
 end
 
-control 'nginx-conf-file' do
+control 'nginx-config-file' do
   impact 1.0
-  title 'NGINX configuration file'
-  desc 'The NGINX config file should exist.'
+  title 'NXINX configuration file'
+  desc 'The NGINX config file should exist as a file'
   describe file('/etc/nginx/nginx.conf') do
     it { should be_file }
   end
@@ -30,8 +36,8 @@ end
 
 control 'nginx-conf-perms' do
   impact 1.0
-  title 'NGINX configuration permissions'
-  desc 'The NGINX config file should owned by root, be writable only by owner, and not writeable or and readable by others.'
+  title 'NGINX configuration'
+  desc 'The NGINX config file should be owned by root, be writable only by owner, and not writeable or and readable by others.'
   describe file('/etc/nginx/nginx.conf') do
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
@@ -44,12 +50,17 @@ end
 control 'nginx-shell-access' do
   impact 1.0
   title 'NGINX shell access'
-  desc 'The NGINX shell access should be restricted to admin users.'
-  non_admin_users = users.shells(/bash/).usernames
-  describe "Shell access for non-admin users" do
-    it "should be removed." do
-      failure_message = "These non-admin should not have shell access: #{non_admin_users.join(", ")}"
-      expect(non_admin_users).to be_in(input('admin_users')), failure_message
-    end
+  desc 'NGINX shell access should be restricted to admin users'
+  describe users.shells(/bash/).usernames do
+    it { should be_in input('admin_users') }
+  end
+end
+
+control 'nginx-interview' do
+  impact 1.0
+  title 'NGINX interview'
+  desc 'NGINX admins should have documentation on security procedures'
+  describe "Manual Review" do
+    skip "This control must be manually reviewed."
   end
 end
